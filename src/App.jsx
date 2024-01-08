53104451f9b0eebc940d27a5e9a1919a7c97a6c7
Nate Mills <natemills018@gmail.com>
2024-01-07 21:14:59 -0500

    Adding eventlistener logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-    return(
-        <div></div>
+
+    const [chirp, setChirp] = useState('')
+    const [chirpSubmitted, setchirpSubmitted] = useState('')
+    const handlechirpSubmit = (e) => {
+
+        setChirp(e.target.value);
+    }
+
+    const sendChirp = e => {
+
+        e.preventDefault();
+
+        setchirpSubmitted({ chirp })
+
+        setChirp('');
+
+    }
+
+    return (
+        <div>
+            <form className='form-group container row col-6 mx-auto border p-3'>
+                <input
+                    className='form-control'
+                    placeholder='Sounds Like a bit of Ostrich'
+                    value={chirp}
+                    onChange={handlechirpSubmit}
+                // onChange={handlechirpSubmit}
+
+                />
+                <button className='btn btn-primary mt-3' onClick={sendChirp}>Chirp...</button>
+            </form>
+
+
+            <div className='text-center'>
+                <h1>{chirpSubmitted}</h1>
+            </div>
+
+        </div>
     )
 }
 
---
+++ src/components/submit.jsx
@@ -0,0 +1,23 @@
+// import React from 'react';
+
+// const submitComponent = props => {
+//     const classes = props.classes
+//     const handleDescription = () => {
+
+//     }
+//     return (
+//         <div className='classes.container'>
+//             <ul className={classes.items}>
+//                 {props.children.map((item, i) => (
+//                     <li key={i} className={classes.item}>
+//                         <p className={classes.category}>
+//                             {item.genre} {item.date}
+//                         </p>
+//                         <p className={classes.header}>{item.header}</p>
+//                         <p className={classes.details}>{item.description}</p>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
